8a85d4581f8fd4b39d8b53b55987246217f060a8
alanifotis <alanifotis@yahoo.com>
2024-05-03 21:27:49 +0200

    Initial Commit

---
+++ My-Api/src/test/java/org/hp/server/api/MyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hp.server.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
